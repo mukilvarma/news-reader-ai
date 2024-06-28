ad0f8621a83514a31b79ff815086cb8d72692ba7
mukilvarma <33450604+mukilvarma@users.noreply.github.com>
2024-06-29 00:17:00 +0530

    Update news_reader.py

--- news_reader.py
+++ news_reader.py
@@ -141,6 +141,4 @@ def play_news():
         return jsonify({'error': 'Failed to fetch news headlines'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(debug=True)
